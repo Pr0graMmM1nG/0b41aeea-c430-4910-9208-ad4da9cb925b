753cf04854668c57b7701677e96b02351e9b829f
Inginer S.P <valdcongroup@gmail.com>
2019-11-27 17:34:05 +0300

    Refactor Function (display_state_screen) split function

--- XO.py
+++ XO.py
@@ -1,10 +1,12 @@
+def show_list(list):
+	string=''
+	for item in list:
+		string+=item+'   '
+	print(string,end='')
 def display_state_screen(state_screen):
 	print('Result:')
 	for i in state_screen:
-		string=''
-		for item in i:
-			string+=item+'   '
-		print(string,end='')
+		show_list(i)
 		print('\n')
 initial_state_screen = [['#','a','b','c'],['a','1','1','1'],['b','2','2','2'],['c','3','3','3']]
 display_state_screen(initial_state_screen)
