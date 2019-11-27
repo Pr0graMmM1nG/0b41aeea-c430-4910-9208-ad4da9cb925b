5e41ed9c939167633a383bf6f37b4ad6ad646229
Inginer S.P <valdcongroup@gmail.com>
2019-11-27 18:18:21 +0300

    Add Function (merge_list) and refactor names

--- XO.py
+++ XO.py
@@ -3,12 +3,24 @@ def show_list(list):
 	for item in list:
 		string+=item+'   '
 	print(string,end='')
-def display_state_screen(state_screen):
+def display_state_screen(matrix_state):
 	print('Result:')
-	for i in state_screen:
+	for i in matrix_state:
 		show_list(i)
 		print('\n')
-initial_state_screen = [['#','a','b','c'],['a','1','1','1'],['b','2','2','2'],['c','3','3','3']]
-display_state_screen(initial_state_screen)
+def merge_row(list1,list2):
+	list1.extend(list2)
+	return list1
+	
+	
+row1=['1','1','1']
+row2=['2','2','2']
+row3=['3','3','3']	
+	
+matrix = [['#','a','b','c'],merge_row(['a'],row1),merge_row(['b'],row2),merge_row(['c'],row3)]
+display_state_screen(matrix)
+print(row1)
+print(row2)
+print(row3)
 
 input("press enter to exit...")
