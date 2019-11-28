68da2cc96768868b692806814a73d49336414995
root <root@localhost.localdomain>
2019-11-28 14:39:32 +0200

    Add Function (GetUserValue) to get value from user and test it

--- Function.py
+++ Function.py
@@ -3,12 +3,26 @@ def show_list(list):
 	for item in list:
 		string+=item+'   '
 	print(string,end='')
+
 def display_state_screen(matrix_state):
 	print('Result:')
 	for i in matrix_state:
 		show_list(i)
 		print('\n')
+
 def merge_row(list1,list2):
 	list1.extend(list2)
 	return list1
+
+def getMatrix(list1,list2,list3):
+	matrix = [['#','a','b','c'],merge_row(['a'],list1),merge_row(['b'],list2),merge_row(['c'],list3)]
+	return matrix
+
+def GetUserValue():
+	userInput= input('Run Player X: ')
+	while len(userInput)!=2:
+		print('Wrong Value: Please enter valid value.')
+		userInput= input('Run Player X: ')
+	else:
+		print('User set: {0}'.format(userInput))
 	
--- XO.py
+++ XO.py
@@ -3,10 +3,8 @@ row1=['1','1','1']
 row2=['2','2','2']
 row3=['3','3','3']	
 
-
-matrix = [['#','a','b','c'],F.merge_row(['a'],row1),F.merge_row(['b'],row2),F.merge_row(['c'],row3)]
+matrix=F.getMatrix(row1,row2,row3)
 F.display_state_screen(matrix)
-print(row1)
-print(row2)
-print(row3)
+F.GetUserValue()
+
 input("press enter to exit...")
