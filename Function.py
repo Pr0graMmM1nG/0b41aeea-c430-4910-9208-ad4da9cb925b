0a7ba74093ba0ed7fcc079a652a631281ced22ba
Inginer S.P <valdcongroup@gmail.com>
2019-11-29 18:08:53 +0300

    Add dohuia functii: ne razberiosh nado refactoriti

--- Function.py
+++ Function.py
@@ -1,5 +1,14 @@
-
-
+def InitMatrix():
+	row1=['*','*','*']
+	row2=['*','*','*']
+	row3=['*','*','*']	
+	matrix=getMatrix(row1,row2,row3)
+	return matrix
+	
+def getMatrix(list1,list2,list3):
+	matrix = [['#','a','b','c'],merge_row(['a'],list1),merge_row(['b'],list2),merge_row(['c'],list3)]
+	return matrix
+	
 def show_list(list):
 	string=''
 	for item in list:
@@ -7,7 +16,7 @@ def show_list(list):
 	print(string,end='')
 
 def display_state_screen(matrix_state):
-	print('Result:')
+	print('Games State:')
 	for i in matrix_state:
 		show_list(i)
 		print('\n')
@@ -15,10 +24,6 @@ def display_state_screen(matrix_state):
 def merge_row(list1,list2):
 	list1.extend(list2)
 	return list1
-
-def getMatrix(list1,list2,list3):
-	matrix = [['#','a','b','c'],merge_row(['a'],list1),merge_row(['b'],list2),merge_row(['c'],list3)]
-	return matrix
 
 def GetRowCol(UserValue):
 	temp = str(UserValue)
@@ -40,8 +45,10 @@ def GetRowCol(UserValue):
 	return 	RowCol	
 
 def InputUser():
+
 	GetUserString=input("Please select Row and Col {0}".format('ex.(ab = row#1 and col#2):'))[:2]
 	return GetUserString
+	
 def SelectPattern (userValue):
 	import re
 	pattern=re.search('[a-c]{2}',userValue)
@@ -55,5 +62,36 @@ def GetUserValue():
 		userValue = InputUser()
 		pattern = SelectPattern(userValue)
 	else:
-		print('User select: {0}'.format(userValue))
+		print('Player selected: {0}'.format(userValue))
 	return userValue
+	
+def ModifyMatrix (Matrix , UserValue, XO):
+	RowAndCol=GetRowCol(UserValue)
+	Matrix[RowAndCol[0]][RowAndCol[1]]=XO
+	return Matrix
+
+def ifExist_UserValue(AllValue, CurrentValue):
+	countValue=0
+	for item in AllValue:
+		if item==CurrentValue:
+			countValue+=1
+	if countValue==0:
+		return bool(0)
+	else:
+		return bool(1)
+def StartGames():
+	print('Begin game: Loading...')
+	
+def CountHods(PlayerX,PlayerO): #Ceva ne to calculeaza
+	x=len(PlayerX)
+	y=len(PlayerO)
+	return x+y
+	
+def NextPlayer(Player):
+	if Player=='X':
+		Player='O'
+	else:
+		Player='X'
+	return Player
+
+		
