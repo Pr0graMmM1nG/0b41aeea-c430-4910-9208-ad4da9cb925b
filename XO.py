a1a0b1e6281ae980552ada2bbd86cff0af2d4ff7
Inginer S.P <valdcongroup@gmail.com>
2019-11-29 14:32:42 +0300

    Refactor Validate() -> GetRowCol()

--- XO.py
+++ XO.py
@@ -2,9 +2,13 @@ import Function as F
 row1=['1','1','1']
 row2=['2','2','2']
 row3=['3','3','3']	
-
 matrix=F.getMatrix(row1,row2,row3)
-F.display_state_screen(matrix)
-F.GetUserValue()
 
+F.display_state_screen(matrix)
+
+userInput = F.GetUserValue()
+
+Find=F.GetRowCol(userInput)
+
+print(Find)
 input("press enter to exit...")
