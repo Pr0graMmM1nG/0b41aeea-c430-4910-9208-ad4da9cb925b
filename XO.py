06292e19ba486b01db1b492e532dab0830932fe4
Inginer S.P <valdcongroup@gmail.com>
2019-11-29 18:09:26 +0300

    Test Games...Multiple Bug Found

--- XO.py
+++ XO.py
@@ -1,14 +1,35 @@
 import Function as F
-row1=['1','1','1']
-row2=['2','2','2']
-row3=['3','3','3']	
-matrix=F.getMatrix(row1,row2,row3)
+F.StartGames()
 
+matrix=F.InitMatrix()
 F.display_state_screen(matrix)
 
-userInput = F.GetUserValue()
+PlayerXValue=[]
+PlayerOValue=[]
+Player='X'
+Count=9
 
-Find=F.GetRowCol(userInput)
+while F.Hods(PlayerXValue,PlayerOValue)<Count:
+	print('Make: {0} pass'.format(F.CountHods(PlayerXValue,PlayerOValue)))
+	print('Player {0}:'.format(Player))
+	
+	userInput = F.GetUserValue()
+	##input logika in caz ca deja asa hod sa facut
+	if F.ifExist_UserValue(PlayerXValue,userInput):
+		print('Value exist please select another value')
+	else:
+		print('No exist:Append value {0} in depositValue'.format(userInput))
+		if Player=='X':
+			PlayerXValue.append(userInput)
+			print('Show value of Player{1}Value: {0}'.format(PlayerXValue,Player))
+		else:
+			PlayerOValue.append(userInput)
+			print('Show value of Player{1}Value: {0}'.format(PlayerOValue,Player))
+	matrix=F.ModifyMatrix(matrix,userInput,Player)
+	F.display_state_screen(matrix)
+	Player=F.NextPlayer(Player)
+	
+	
+	
 
-print(Find)
 input("press enter to exit...")
